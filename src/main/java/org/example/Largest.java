498a64a3d34531402d8cc722e6caf54c7bb4a3de
dzuura <daffa3704@gmail.com>
2025-03-11 21:51:43 +0700

    pertemuan2

--- src/main/java/org/example/Largest.java
+++ src/main/java/org/example/Largest.java
@@ -13,3 +13,4 @@ public class Largest {
         return max;
     }
 }
+
--- src/test/java/LargestTest.java
+++ src/test/java/LargestTest.java
@@ -8,16 +8,4 @@ public class LargestTest {
         int[] arr = { 1, 2, 3, 4, 5 };
         Assertions.assertEquals(5, Largest.largest(arr));
     }
-
-//    @Test
-//    void test() {
-//        Wallet wallet = new Wallet();
-//        Wallet bank = new Wallet();
-//        wallet.addMoney(0,2);
-//        bank.addMoney(0,2);
-////        int a = 1;
-////        a = b;
-////        b = a;
-//        Assertions.assertSame(bank.getTotalMoney(),wallet.getTotalMoney());
-//    }
 }
