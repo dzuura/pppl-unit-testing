7be2451a342d0c81eb681191047e9b8a4a67095a
dzuura <daffa3704@gmail.com>
2025-03-12 14:17:26 +0700

    pertemuan4

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Book {
+    String title;
+    String isbn;
+    String author;
+
+    public Book(String title, String isbn, String author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+}
---
+++ src/main/java/org/example/LibraryService.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryService {
+
+    public List<Book> getAllBooks() {
+        // membaca sesuatu dari db atau API
+        return new ArrayList<>();
+    }
+
+    public void storeData(List<Book> books) {
+        // store data ke db atau API
+    }
+}
